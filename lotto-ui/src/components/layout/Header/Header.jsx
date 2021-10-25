4579ba968c259002a63de613bb23939d90a74d8d
crescendolll <tensaemamo@protonmail.com>
2021-10-25 14:57:41 +0200

    started header + footer w tailwind

--- lotto-ui/src/components/layout/Header/Header.jsx
+++ lotto-ui/src/components/layout/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export const Header = () => {
-    return (
-        <div>
-            header
-        </div>
-    )
+  return (
+    <div className='bg-hellgrau my-9 font-bold text-primary flex justify-between px-6 items-center'>
+      <div className='h-16 w-16 bg-primary hover:bg-primary_neon'>logo</div>
+      <nav className='content-around'>
+        <ul className='text-sm flex px-2 '>
+          <li className='pr-3 hover:text-primary_neon'>
+            <a href='#'>Konto</a>
+          </li>
+          <li className='pr-3 hover:text-primary_neon'>
+            <a href='#'>Tipps abgeben</a>
+          </li>
+          <li className='hover:text-primary_neon'>
+            <a href='#'>Statistik</a>
+          </li>
+        </ul>
+      </nav>
+      <a className='w-16 bg-primary text-hellgrau px-4 py-2 rounded hover:bg-primary_neon' href='#'>
+        Kontoverwaltung
+      </a>
+    </div>
+  )
 }
