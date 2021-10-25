3a21e256cd163ad1fd93b41a64d040367ddff63d
crescendolll <tensaemamo@protonmail.com>
2021-10-25 14:57:16 +0200

    tailwind font customization

--- lotto-ui/tailwind.config.js
+++ lotto-ui/tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2F1160',
+        primary_neon: '#6200FF',
+        dunkelgrau: '#B9B7BD', 
+        grau: '#EBE9F0',
+        hellgrau: '#FBFAFC',
+      },
+      fontFamily:{
+          sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
