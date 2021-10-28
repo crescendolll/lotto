fa49a91dc1fdf56c4a2a4af4a338c5f3d412fe4f
crescendolll <tensaemamo@protonmail.com>
2021-10-28 10:21:18 +0200

    changed empty hrefs for eslint

--- lotto-ui/src/components/layout/Footer/Footer.jsx
+++ lotto-ui/src/components/layout/Footer/Footer.jsx
@@ -5,14 +5,14 @@ export const Footer = () => {
     <div className='bg-hellgrau text-primary text-sm grid justify-items-center space-y-8'>
       <div className='pt-8'>logo</div>
       <div className='bg-hellgrau p-4 rounded space-x-8'>
-        <a href='#' className=''>For Customers</a>
-        <a href='#'>For Employers</a>
+        <a href='/' className=''>For Customers</a>
+        <a href='/'>For Employers</a>
       </div>
       <div> 
           socials
       </div>
       <div className='text-xs pb-20'>
-        Coded with React and Go and a lot of ❤ in Berlin ©2021 Lotto-App{' '}
+        Coded with React and Go and a lot of ❤ in Berlin ©2021 Lotto-App
       </div>
     </div>
   )
