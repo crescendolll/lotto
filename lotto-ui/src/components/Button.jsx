9fe100fbb08f3821405cdf0774dcb1e0ff3b8afd
crescendolll <tensaemamo@protonmail.com>
2021-12-02 21:17:16 +0100

    fix so that onClick is passed as props

--- lotto-ui/src/components/Button.jsx
+++ lotto-ui/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = (props) => {
   return (
-    <button className='bg-primary text-hellgrau px-4 py-2 rounded hover:bg-primary_neon'>
+    <button {...props} className='bg-primary text-hellgrau px-4 py-2 rounded hover:bg-primary_neon'>
       {props.children}
     </button>
   )
