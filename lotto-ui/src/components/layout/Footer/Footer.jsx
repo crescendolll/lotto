fe14571c7f40f422d713b09c660579fceed7964c
crescendolll <tensaemamo@protonmail.com>
2021-12-03 11:27:05 +0100

    included logo.svg

--- lotto-ui/src/components/layout/Footer/Footer.jsx
+++ lotto-ui/src/components/layout/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ReactComponent as Logo} from '../../../lotto_logo.svg'
+import { Link } from '../../Link'
+
 
 export const Footer = () => {
   return (
     <div className='bg-hellgrau text-primary text-sm grid justify-items-center space-y-8'>
-      <div className='pt-8'>logo</div>
+      <Link link='/'><Logo title='Kleeblatt' className='w-44 align-center mt-8' /></Link>
       <div className='bg-hellgrau p-4 rounded space-x-8'>
         <a href='/' className=''>For Customers</a>
         <a href='/'>For Employers</a>
