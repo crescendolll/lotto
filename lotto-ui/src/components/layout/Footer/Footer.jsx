00752e63c8525ab60f45d050159f91521cd13f18
crescendolll <tensaemamo@protonmail.com>
2021-12-03 17:41:07 +0100

    smaller logo

--- lotto-ui/src/components/layout/Footer/Footer.jsx
+++ lotto-ui/src/components/layout/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import { Link } from '../../Link'
 export const Footer = () => {
   return (
     <div className='bg-hellgrau text-primary text-sm grid justify-items-center space-y-8'>
-      <Link link='/'><Logo title='Kleeblatt' className='w-44 align-center mt-8' /></Link>
+      <Link link='/'><Logo title='Kleeblatt' className='w-44 align-center mt-8 p-4' /></Link>
       <div className='bg-hellgrau p-4 rounded space-x-8'>
         <a href='/' className=''>For Customers</a>
         <a href='/'>For Employers</a>
