4e32daf89312fa8d32325677c9dfa25f51d0e9b5
crescendolll <tensaemamo@protonmail.com>
2021-12-03 14:43:47 +0100

    added margin

--- lotto-ui/src/components/Button.jsx
+++ lotto-ui/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = (props) => {
   return (
-    <button {...props} className='bg-primary text-hellgrau px-4 py-2 rounded hover:bg-primary_neon'>
+    <button {...props} className='bg-primary text-hellgrau px-4 py-2 rounded m-2 hover:bg-primary_neon'>
       {props.children}
     </button>
   )
