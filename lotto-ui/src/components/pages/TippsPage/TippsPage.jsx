fc40a91050116f497f923662458e38c9f8e07296
crescendolll <tensaemamo@protonmail.com>
2021-12-03 17:53:58 +0100

    implements submitting a tip

--- lotto-ui/src/components/pages/TippsPage/TippsPage.jsx
+++ lotto-ui/src/components/pages/TippsPage/TippsPage.jsx
@@ -4,7 +4,7 @@ import { Footer } from '../../layout/Footer/Footer'
 import { TippsTabelle } from '../../forms/TippsTabelle/TippsTabelle'
 import { TippabgabeButton } from '../../forms/TippabgabeButton/TippabgabeButton'
 import { AnsichtSwitch } from '../../forms/AnsichtSwitch/AnsichtSwitch'
-import { getTips, getOpenGames, getClosedGames } from '../../../api'
+import { getTips, getOpenGames, getClosedGames, submitTip } from '../../../api'
 import { Button } from '../../Button'
 
 export const TippsPage = ({ auth, onLogout }) => {
@@ -13,6 +13,11 @@ export const TippsPage = ({ auth, onLogout }) => {
   const [tiplist, setTips] = useState([])
 
   const [openGames, setOpenGames] = useState([])
+
+  const [date, setDate] = useState('')
+  const [tipsubmission, setTipsubmission] = useState('')
+
+  const [showTipSubmit, setShowTipSubmit] = useState(false)
 
   useEffect(() => {
     getTips(auth, '', '').then((data) => {
@@ -27,14 +32,56 @@ export const TippsPage = ({ auth, onLogout }) => {
     })
   }
 
+  const onOpenSubmitTip = () => {
+    setShowTipSubmit(true)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    submitTip(auth, tipsubmission, date).then((data) => console.log(data))
+    setShowTipSubmit(false)
+    setDate('')
+    setTipsubmission('')
+  }
+
+  const onChangeDate = (event) => {
+    setDate(event.target.value)
+  }
+
+  const onChangeTipsubmission = (event) => {
+    setTipsubmission(event.target.value)
+  }
+
   return (
     <div>
-      <Header auth={auth} onLogout={onLogout}/>
+      <Header auth={auth} onLogout={onLogout} />
       <Button onClick={() => onGetOpenGames(auth)}>get open Games</Button>
+
+      {showTipSubmit ? (
+        <form onSubmit={onSubmit} className='my-9 bg-grau max-w-min min-w-max p-2'>
+          <Button>Submit</Button>
+          <input
+            type='date'
+            size='20'
+            onChange={onChangeDate}
+            className=' mx-2 border rounded-md border-dunkelgrau'
+          />
+          <input
+            type='text'
+            size='20'
+            onChange={onChangeTipsubmission}
+            className=' mx-2 border rounded-md border-dunkelgrau'
+          />
+        </form>
+      ) : (
+        <div className='bg-grau p-2 max-w-min min-w-max my-9'>
+          <Button onClick={onOpenSubmitTip}>+</Button> Submit a Tip
+        </div>
+      )}
       {openGames.length > 0 ? <TippsTabelle tips={openGames} /> : null}
       <AnsichtSwitch />
       <TippsTabelle tips={tiplist} />
-      <Footer />               
+      <Footer />
     </div>
   )
 }
