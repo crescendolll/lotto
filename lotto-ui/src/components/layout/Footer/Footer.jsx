959f13e1983344c348b1041ce92177faabb26520
crescendolll <tensaemamo@protonmail.com>
2021-12-05 14:18:08 +0100

    fixed footer to bottom of page

--- lotto-ui/src/components/layout/Footer/Footer.jsx
+++ lotto-ui/src/components/layout/Footer/Footer.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import { ReactComponent as Logo} from '../../../lotto_logo.svg'
+import { ReactComponent as Logo } from '../../../lotto_logo.svg'
 import { Link } from '../../Link'
-
 
 export const Footer = () => {
   return (
-    <div className='bg-hellgrau text-primary text-sm grid justify-items-center space-y-8'>
-      <Link link='/'><Logo title='Kleeblatt' className='w-44 align-center mt-8 p-4' /></Link>
-      <div className='bg-hellgrau p-4 rounded space-x-8'>
-        <a href='/' className=''>For Customers</a>
-        <a href='/'>For Employers</a>
-      </div>
-      <div> 
-          socials
-      </div>
-      <div className='text-xs pb-20'>
-        Coded with React and Go and a lot of ❤ in Berlin ©2021 Lotto-App
+    <div className='bottom-0 fixed w-full'>
+      <div className='bg-hellgrau text-primary text-sm grid justify-items-center space-y-8 '>
+        <Link link='/'>
+          <Logo title='Kleeblatt' className='w-44 align-center mt-8 p-4' />
+        </Link>
+        <div className='bg-hellgrau p-4 rounded space-x-8'>
+          <a href='/' className=''>
+            For Customers
+          </a>
+          <a href='/'>For Employers</a>
+        </div>
+        <div>socials</div>
+        <div className='text-xs pb-20'>
+          Coded with React and Go and a lot of ❤ in Berlin ©2021 Lotto-App
+        </div>
       </div>
     </div>
   )
